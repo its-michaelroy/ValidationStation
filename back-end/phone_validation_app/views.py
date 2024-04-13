043e996c05cdc9e8b1ed8ed41a13a9125421c761
Michael Roy <michaelr.social@skiff.com>
2024-04-12 20:12:22 -0400

    thunderstorm

--- back-end/phone_validation_app/views.py
+++ back-end/phone_validation_app/views.py
@@ -21,31 +21,53 @@ import json
 # Create your views here.
 class PhoneValidation(TokenReq):
     def post(self, request):
-        print('UNFORMAT REQUEST.BODY!!!!!!!',request.body)
         body = json.loads(request.body)
-        print('AFTER THE JSON.LOADS (JUST BODY)!!!!!!!',body)
         # Grab the phone number from the request body below
         number = body['phone_number']
         countryCode = body['countryCode']
-        #NOPE > internationalFormat = body['internationalFormat']
-        print('YO ACCESS NUMBER VALUE FROM PHONE_NUM KEY!!!!!!!',number, countryCode)
+        print('ACCESS NUMBER VALUE FROM PHONE_NUM KEY!!!!!!!',number, countryCode)
         API_KEY = env.get('API_KEY')
         #make sure the number is in the correct format
         adjusted_number = number[0] + number[1] + number[2]+"+" + number[3] + number[4] + number[5]+ "-" + number[6] + number[7] + number[8] + number[9]
-        #make the request to the API
+        #COnstruct request to the API
         endpoint = f"https://api-bdc.net/data/phone-number-validate?number={adjusted_number}&countryCode={countryCode}&localityLanguage=en&key={API_KEY}"
         response = requests.get(endpoint)
         print('LOOK A POTATO CANDY RECIPE', response)
-        data = request.data.copy()
-        print('DATA IS HERE >>>', data)
-        print('RESPONSE IS HERE >>>', response)
+        data_copy = request.data.copy() #copy of data from above
+        print('DATA IS HERE >>>', data_copy
+        print('RESPONSE IS HERE >>>', response) # just 200
+        if response.status_code == 200:
+            data = response.json()
+            print('DATA IS HERE >>>', data)
+
+            #create & save the phone object
+            new_phone = Phone(**data)
+            new_phone.save()
+            return Response({'phone': new_phone.phone_number}, status=HTTP_201_CREATED)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             # data['user'] = request.user.id
         # new_phone = Phone(**data)
         # try:
         #     new_phone.full_clean()
         #     new_phone.save()
             # return Response({'phone': new_phone.phone_number}, status=HTTP_201_CREATED)
-        return Response({'phone': response}, status=HTTP_201_CREATED)
+        # return Response({'phone': response}, status=HTTP_201_CREATED)
         # except ValidationError as e:
         #     return Response(e, status=HTTP_400_BAD_REQUEST)
 
