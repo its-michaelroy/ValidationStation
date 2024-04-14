b00529bb412348f61ab968609b36c789ff3e57d2
Michael Roy <michaelr.social@skiff.com>
2024-04-14 18:26:39 -0400

    outages, making backup in case still

--- back-end/email_validation_app/urls.py
+++ back-end/email_validation_app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import EmailValidation, A_email_record, All_email_records
-#, Whitelist_email_records, Blacklist_email_records
+from .views import EmailValidation, A_email_record, All_email_records, Whitelist_email_records, Blacklist_email_records
 
 urlpatterns = [
     path('', EmailValidation.as_view(), name='email_validation'),
@@ -8,7 +7,6 @@ urlpatterns = [
     path('delete/<str:identifier>/', A_email_record.as_view(), name='delete_email_record'),
     path('update/<str:identifier>/', A_email_record.as_view(), name='update_email_record'),
     path('all/', All_email_records.as_view(), name='all_email_records'),
-    # path('whitelist/', Whitelist_email_records.as_view(), name='whitelist_email_records'),
-    # path('blacklist/', Blacklist_email_records.as_view(), name='blacklist_email_records'),
-
+    path('whitelist/', Whitelist_email_records.as_view(), name='whitelist_email_records'),
+    path('blacklist/', Blacklist_email_records.as_view(), name='blacklist_email_records'),
 ]
